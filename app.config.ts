a692ae28486fe1b90a7072bbfd44d5cf402c68c9
Rahul Vijay <111470767+rahulvijay5@users.noreply.github.com>
2025-03-11 11:39:16 +0530

    update in app.config.ts for dynamic name and identifier

--- app.config.ts
+++ app.config.ts
@@ -1,37 +1,38 @@
 import "dotenv/config";
+import { ConfigContext, ExpoConfig } from "expo/config";
 
-const IS_DEV = process.env.APP_VARIANT === 'development';
-const IS_PREVIEW = process.env.APP_VARIANT === 'preview';
+const IS_DEV = process.env.APP_VARIANT === "development";
+const IS_PREVIEW = process.env.APP_VARIANT === "preview";
 
 const getUniqueIdentifier = () => {
   if (IS_DEV) {
-    return 'site.rahulvijay.travelone.dev';
+    return "site.rahulvijay.travelone.dev";
   }
 
   if (IS_PREVIEW) {
-    return 'site.rahulvijay.travelone.preview';
+    return "site.rahulvijay.travelone.preview";
   }
 
-  return 'site.rahulvijay.travelone';
+  return "site.rahulvijay.travelone";
 };
 
 const getAppName = () => {
   if (IS_DEV) {
-    return 'TravelOne (Dev)';
+    return "TravelOne (Dev)";
   }
 
   if (IS_PREVIEW) {
-    return 'TravelOne (Preview)';
+    return "TravelOne (Preview)";
   }
 
-  return 'TravelOne';
+  return "TravelOne";
 };
 
-
-const config = {
-  name: "TravelOne",
-  slug: "travelone",
-  version: "1.0.0",
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: getAppName(),
+  slug: "tavelone",
+  version: "1.0.0", 
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: "travelone",
@@ -39,22 +40,22 @@ const config = {
   newArchEnabled: true,
   ios: {
     supportsTablet: true,
-    bundleIdentifier: "site.rahulvijay.travelone",
+    bundleIdentifier: getUniqueIdentifier(),
     infoPlist: {
-      ITSAppUsesNonExemptEncryption: false
-    }
+      ITSAppUsesNonExemptEncryption: false,
+    },
   },
   android: {
     adaptiveIcon: {
       foregroundImage: "./assets/images/icon.png", // Make sure this has transparency
-      backgroundColor: "#ffffff"
+      backgroundColor: "#ffffff",
     },
-    package: "site.rahulvijay.travelone"
+    package: getUniqueIdentifier(),
   },
   web: {
     bundler: "metro",
     output: "static",
-    favicon: "./assets/images/icon.png"
+    favicon: "./assets/images/icon.png",
   },
   plugins: [
     "expo-router",
@@ -64,33 +65,20 @@ const config = {
         image: "./assets/images/splash-icon.png",
         imageWidth: 200,
         resizeMode: "contain",
-        backgroundColor: "#ffffff"
-      }
-    ]
+        backgroundColor: "#ffffff",
+      },
+    ],
   ],
   experiments: {
-    typedRoutes: true
+    typedRoutes: true,
   },
   extra: {
     router: { origin: false },
-    eas: { projectId: "d082d275-f1c5-4b61-a1a6-026ba127a452" },
+    eas: { projectId: "fc0a62ef-a6cf-455e-b697-2c6ff07d4fbf" },
     CLERK_KEY: process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY,
     API_URL: process.env.EXPO_PUBLIC_API_URL,
     APP_URL: process.env.EXPO_PUBLIC_APP_URL,
-    SECURITY_KEY: process.env.EXPO_PUBLIC_SECURITY_KEY
+    SECURITY_KEY: process.env.EXPO_PUBLIC_SECURITY_KEY,
   },
-  owner: "rahulvijay"
-};
-
-export default ({ config }) => ({
-  ...config,
-  name:getAppName(),
-  ios: {
-    ...config.ios,
-    bundleIdentifier: getUniqueIdentifier(),
-  },
-  android: {
-    ...config.android,
-    package: getUniqueIdentifier(),
-  },
+  owner: "rahulvijay",
 });
